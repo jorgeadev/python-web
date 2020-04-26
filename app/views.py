3c4cde124ed7d433168adba01a1fafee5f19f258
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-04-26 02:17:58 -0500

    Create views.py

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint
+from flask import render_template, request
+from .forms import LoginForm
+
+page = Blueprint("page", __name__)
+
+
+@page.app_errorhandler(404)
+def page_not_found(error):
+	return render_template('errors/404.html'), 404
+
+
+@page.route("/")
+def index():
+	return render_template("index.html", title = "Nuevo titulo de la pagina de Jorge")
+
+
+@page.route("/login", methods = ['GET', 'POST'])
+def login():
+	form = LoginForm(request.form)
+
+	if (request.method == 'POST') and (form.validate()):
+		print(form.username.data)
+		print(form.password.data)
+		print("Nueva sesión creada")
+
+	return render_template("auth/login.html", title = "Login", form = form)
